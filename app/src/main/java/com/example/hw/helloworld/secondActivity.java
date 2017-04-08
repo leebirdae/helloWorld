dace7e5b1448ac32c72363e89df44df98614a8b6
leebirdae <Watklm4@gmail.com>
2017-04-08 00:13:41 -0400

    First Commit

--- app/src/main/java/com/example/hw/helloworld/secondActivity.java
+++ app/src/main/java/com/example/hw/helloworld/secondActivity.java
@@ -20,7 +20,7 @@ public class secondActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, getString(R.string.snack), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
