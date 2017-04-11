de7d9073fb6fa8c7fbd779893881164037711397
leebirdae <Watklm4@gmail.com>
2017-04-10 21:48:05 -0400

    actionbar up nav

--- app/src/main/java/com/example/hw/helloworld/secondActivity.java
+++ app/src/main/java/com/example/hw/helloworld/secondActivity.java
@@ -3,6 +3,7 @@ package com.example.hw.helloworld;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -13,8 +14,11 @@ public class secondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
